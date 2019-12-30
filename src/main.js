823a3912c98839236c0faa748aebf239ffac5952
Alan-peak <18813291795@163.com>
2019-12-31 01:58:41 +0800

    添加axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+// 配置请求的根路径
+axios.default.baseURL = 'http://127.0.0.1:8080/mcs/'
+Vue.prototype.$http = axios
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
